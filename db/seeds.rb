0cef75a8db4ec82489e020c5dd7806e2aa7f95f0
Mike Liu <mikesholiu@gmail.com>
2015-04-11 17:06:13 -0700

    Seed Data Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
  require 'faker'
  
  # Create Posts
- 50.times do
+ 5.times do
    Post.create!(
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
  end
+ post_contents = {title: "Unique title 10", body: "Unique body 20"}
+ Post.create(post_contents) unless Post.where(post_contents).first
+
  posts = Post.all
+
  
  # Create Comments
  100.times do
@@ -20,3 +24,4 @@
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+
