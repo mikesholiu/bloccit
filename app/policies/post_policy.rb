ce6d10fb9babbcd4e1618683ca3082dc4ff665a7
Mike Liu <mikesholiu@gmail.com>
2015-05-10 07:55:21 -0700

    policy scope

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,25 @@ def index?
   true
 end
 
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
+  end
+
+  def update?
+    user.admin? or not post.published?
+  end
+
 end
