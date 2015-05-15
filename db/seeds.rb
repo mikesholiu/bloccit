9ee10c5db7d493e98276f68ff6adf23d79a780d7
Mike Liu <mikesholiu@gmail.com>
2015-05-15 10:16:24 -0700

    topics-and-posts checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,10 +20,19 @@
  
  # The `save` method then saves this User to the database.
 
+ 15.times do
+  Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+  end
+  topics = Topic.all
+
  # Create Posts
  50.times do
    Post.create!(
      user: users.sample,
+      topic: topics.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -40,12 +49,33 @@
  end
 
 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'mikesholiu@gmail.com',
-   password: 'testpassword'
+
+ admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+  )
+ admin.skip_confirmation!
+ admin.save!
+
+  moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
  )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+ member.skip_confirmation!
+ member.save!
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
