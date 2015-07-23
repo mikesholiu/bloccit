2ee093cb12517724d462642443bc98213bb7d993
Mike Liu <mikesholiu@gmail.com>
2015-07-22 22:34:48 -0700

    removed head conflicts

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,22 +1,5 @@
  require 'rails_helper'
-<<<<<<< HEAD
- 
- describe "Visiting profiles" do
- 
-   include TestFactories
- 
-   before do
-     @user = authenticated_user
-     @post = associated_post(user: @user)
-     @comment = Comment.new(user: @user, post: @post, body: "A Comment")
-     allow(@comment).to receive(:send_favorite_emails)
-     @comment.save!
-   
-   end
- 
-   describe "not signed in" do
- 
-=======
+
 
  describe "Visiting profiles" do
 
@@ -31,8 +14,6 @@
    end
 
    describe "not signed in" do
-
->>>>>>> public-profiles
      it "shows profile" do
        visit user_path(@user)
        expect(current_path).to eq(user_path(@user))
@@ -40,13 +21,8 @@
        expect( page ).to have_content(@user.name)
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
-<<<<<<< HEAD
 
      end
  
-=======
      end
-
->>>>>>> public-profiles
    end
- end
