d8664e07d00b3ddda335a1e1632b5808d83f7383
Mike Liu <mikesholiu@gmail.com>
2015-07-22 22:46:21 -0700

    public profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -23,6 +23,23 @@
        expect( page ).to have_content(@comment.body)
 
      end
- 
-     end
+    end
+   
+    describe "user visiting their own profile" do
+      
+      before do
+        login_as(@user, scope: :user)
+      end
+
+      it "shows profile" do
+       visit user_path(@user)
+       expect(current_path).to eq(user_path(@user))
+
+       expect( page ).to have_content(@user.name)
+       expect( page ).to have_content(@post.title)
+       expect( page ).to have_content(@comment.body)
+    end
    end
+end
+
+
