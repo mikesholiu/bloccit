758ab6e338788e8f5e35ce2f82c0b89781f8e595
Mike Liu <mikesholiu@gmail.com>
2015-07-26 08:45:16 -0700

    creatin gwith ajax checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,24 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
     @comments = @post.comments
-    @comment = current_user.comments.build(params.require(:comment).permit(:body))
+
+    @comment = Comment.new( comment_params )
+    @comment.user = current_user
     @comment.post = @post
+    @new_comment = Comment.new
 
     authorize @comment
-    
+
     if @comment.save
-     flash[:notice] = "Comment was posted."
-     redirect_to [@topic, @post]
+      flash[:notice] = "Comment was created."
     else
-      flash[:error] = "There was an error posting the comment. Please try again."
-      redirect_to [@topic, @post]
+      flash[:error] = "There was an error saving the comment. Please try again."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
@@ -38,6 +43,13 @@ class CommentsController < ApplicationController
       format.js
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+  
 end
 
 
